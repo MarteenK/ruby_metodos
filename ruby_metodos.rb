0c66cf3ba422fac5cd928042446718f217efa50d
Martin Karlsson <83670863+MarteenK@users.noreply.github.com>
2022-10-22 19:02:05 +0100

    vbhj

---
+++ ruby_metodos.rb
@@ -0,0 +1,15 @@
+#Ejercicio - Enviando mensajes
+#Rolando desea mandar el mismo mensaje a diferentes personas, únicamente cambiaría el nombre, sin embargo no quiere estar escribiendo el mismo texto ¿Cómo le ayudarías?
+#Estos son los mensajes que tiene que mandar:
+
+#=> Hola Carlos te estamos esperando! Soy Rolando.
+#=> Hola Manuel te estamos esperando! Soy Rolando.
+#=> Hola Gaby te estamos esperando! Soy Rolando.
+#=> Hola Samy te estamos esperando! Soy Rolando.
+#=> Hola Yadira te estamos esperando! Soy Rolando.
+#=> Hola Libis te estamos esperando! Soy Rolando.
+#=> Hola Yoli te estamos esperando! Soy Rolando.
+#=> Hola Faby te estamos esperando! Soy Rolando.
+#=> Hola Moni te estamos esperando! Soy Rolando.
+#=> Hola Pamela te estamos esperando! Soy Rolando.
+
